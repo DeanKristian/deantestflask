24be237b4b84fff39444de73aa2277f95a9df35c
deankristianb <kristian.bardeloza@obf.ateneo.edu>
2017-12-13 12:38:08 +0800

    trial with username and email

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/welcome')
-def welcome():
-    return 'Welcome!'
-
+from views import *
 
 if __name__ == '__main__':
     app.run()
---
+++ views.py
@@ -0,0 +1,15 @@
+from flask import render_template, redirect, url_for, request
+from test import app
+from models import User
+
+
+@app.route('/')
+def index():
+    myUser = User.query.all()
+    return render_template('index.html', t_myUser=myUser)
+
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    User(request.form['username'], request.form['email'])
+    return redirect(url_for(index))
